60099eaf3fbdf74098279b8af558b1a42ef4ffd9
Giovana Morais <giovana.vmorais@gmail.com>
2018-04-11 11:11:32 -0300

    sigmoid.m implementado

---
+++ 587591_ex03/funcaoCustoReg.m
@@ -0,0 +1,33 @@
+function [J, grad] = funcaoCustoReg(theta, X, y, lambda)
+%FUNCAOCUSTOREG Calcula o custo da regressao logistica com regularizacao
+%   J = FUNCAOCUSTOREG(theta, X, y, lambda) calcula o custo de usar theta 
+%   como parametros da regressao logistica para ajustar os dados de X e y 
+
+% Initializa algumas variaveis uteis
+m = length(y); % numero de exemplos de treinamento
+
+% Voce precisa retornar as seguintes variaveis corretamente
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== ESCREVA O SEU CODIGO AQUI ======================
+% Instrucoes: Calcule o custo de uma escolha particular de theta.
+%             Voce precisa armazenar o valor do custo em J.
+%             Calcule as derivadas parciais e encontre o valor do gradiente
+%             para o custo com relacao ao parametro theta
+%
+% Obs: grad deve ter a mesma dimensao de theta
+%
+
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+end
