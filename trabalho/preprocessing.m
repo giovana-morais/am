fa38647bbf0edcaee6cb3c33648e72adee3f70d0
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-04-27 18:49:59 -0300

    retirada de duplicatas

--- trabalho/preprocessing.m
+++ trabalho/preprocessing.m
@@ -1,23 +1,24 @@
 % leitura dos dados de treinamento
 train_data = load("-ascii", "dataset_uci/final_X_train.txt");
 
-% leitura das classes de treinamento
-train_classes = load("-ascii", "dataset_uci/final_Y_train.txt");
-
-% une dados de treinamento com classes de treinamento
-train_data = [train_data, train_classes];
-
 % leitura dos dados de teste
 test_data = load("-ascii", "dataset_uci/final_X_test.txt");
+
+% mantem todos os dados de X em apenas uma matriz
+X_data = [train_data; test_data];
+
+% leitura das classes de treinamento
+train_classes = load("-ascii", "dataset_uci/final_Y_train.txt");
 
 % leitura das classes de teste
 test_classes = load("-ascii", "dataset_uci/final_Y_test.txt");
 
-% une dados de teste com classes de teste
-test_data = [test_data, test_classes];
+% mantem todos os dados de Y em apenas uma matriz
+Y_data = [train_classes; test_classes];
 
-% mantem todos os dados em apenas uma matriz
-all_data = [train_data; test_data];
+% seleciona apenas as amostras nao repetidas sem os elementos Y (sem duplicacao de classes)
+[elem, ind] = unique(X_data, 'rows');
 
-% seleciona apenas as amostras nao repetidas da matriz
-A = unique(all_data, 'rows');
+% concatena matriz de amostra X com suas classes
+all_data = [elem, Y_data(ind)];
+
