aef7aa7a9d64c4363db0994d3f2b68af9820f3d2
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-05-08 13:16:34 -0300

    knn ta ridiculo

--- IMPLEMENTACAO/exec_project.m
+++ IMPLEMENTACAO/exec_project.m
@@ -1,6 +1,7 @@
 % Grupo 7 - Reconhecimento de Atividades Humanas
 
 printf("Iniciando execucao.\n");
+clear all, clc, close all;
 % inicialmente ja fizemos pre-processamento dos dados para tentar diminuir a dimensão de atributos e amostras
 % retirando inconsistencias, redundancias, células nulas e fazendo a normalizacao de valores
 % agora recuperamos os dados pre_processados que foram salvos no arquivo pre_processed
@@ -12,4 +13,29 @@ load("pre_processed.mat");
 % em seguida, devemos dividir os dados gerais para validacão cruzada, com k sendo 10 (mais usualmente utilizado em AM)
 % assim sendo, 9 partes irao para treinamento enquanto apenas 1 ira para teste (isso ocorre 10 vezes, para cada algoritmo)
 % aqui conseguiremos avaliar as hipóteses e selecionar a melhor 
+
+ksize = floor(length(all_data)/10);
+ktrain = all_data(1:ksize*9, :);
+ktest = all_data(ksize*9:end, :);
+
+printf('\nIniciando execucao do knn\n');
+fflush(stdout);
+
+% para escolher o k utilizamos o elbow method com k indo de 1 a 20
+% consequentemente escolhemos o k minimo
+% isso aqui vai demorar absurdos, to sem coragem no momento
+for k = 1:20
+  printf("\nPara k = %d\n", k);
+  fflush(stdout);
+  for i = 1:length(ktest)
+    ypred = knn(ktrain(:,1:end-1), ktrain(:,end), ktest(i,1:end-1), k);
+    if(ypred != ktest(i, end))
+      error(i) = 1;
+    endif
+  endfor
+  errorknn(k) = sum(error);
+  printf("Ocorrem %d erros\n", errorknn(k));
+  fflush(stdout);
+endfor
+
 
--- IMPLEMENTACAO/knn.m
+++ IMPLEMENTACAO/knn.m
@@ -1,3 +1,29 @@
+function ypred = knn(train, train_labels, test, K) 
+% Implementacao do knn para o problema de reconhecimento de atividades humanas
 
+% como ja temos os dados normalizados, devemos primeiramente calcular as distancias euclidianas
+% entre os elementos da base de treinamento com os elementos da base de dados
 
+ind_neigh = zeros(K,1);  
+vec_labels = zeros(K,1);
+D = zeros(length(train),1);
 
+for i = 1:length(train)
+	D(i) = norm((train(i,:) - test(1,:)), 2);
+endfor
+
+for aux = 1:K
+  dist = intmax('int32');
+	for i = 1:length(train)
+		if ~ismember(i, ind_neigh) && D(i,1) < dist
+		  dist = D(i,1);
+		  neigh = i;
+		endif 
+	endfor
+  ind_neigh(aux,1) = neigh;
+  vec_labels(aux,1) = train_labels(neigh,1); 
+endfor
+
+ypred = mode(vec_labels);
+
+end
