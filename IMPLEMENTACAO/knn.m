a7579de63a785d9bb682f425e0d6a4dcaf3ef1e6
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-06-01 18:27:33 -0300

    comentei o knn melhorzinho

--- IMPLEMENTACAO/knn.m
+++ IMPLEMENTACAO/knn.m
@@ -1,22 +1,21 @@
 function ypred = knn(train, train_labels, test, K) 
 % Implementacao do knn para o problema de reconhecimento de atividades humanas
 
-% como ja temos os dados normalizados, devemos primeiramente calcular as distancias euclidianas
-% entre os elementos da base de treinamento com os elementos da base de dados
-
-ind_viz = zeros(K,1);  
-% vec_labels = zeros(K,1);
+% inicializa vetor de indices dos vizinhos mais proximos
+ind_viz = zeros(K,1);
+% inicializa vetor de distancias
 D = zeros(length(train),1);
 
+% calcula distancia euclidiana entre todos elementos da amostra de treino com a amostra de teste
 for i = 1:length(train)
 	D(i) = norm((train(i,:) - test(1,:)), 2);
 endfor 
 
 % pega os K menores objetos do vetor de distancias
-%ind_viz = find(D == min(D), K)
 [temp, idx] = sort(D);
 ind_viz = train_labels(idx(1:K));
 
+% pega a moda dos indices dos K vizinhos mais proximos para predizer o resultado
 ypred = mode(ind_viz);
 
 % =========================================================================
