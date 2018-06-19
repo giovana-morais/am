d3fed0a7f3435f8967a96d43beca18dcd7e45995
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-06-18 23:35:54 -0300

    ajeitando bug

--- IMPLEMENTACAO/plotCost.m
+++ IMPLEMENTACAO/plotCost.m
@@ -1,8 +1,8 @@
 function plotCost(costtest, costtrain, title_plot)
   
-  figure, plot(1:rows(predtest), costtest, 'r');
+  figure, plot(1:rows(costtest), costtest, 'r');
   hold on;
-  plot(1:rows(predtest), costtrain, 'b');
+  plot(1:rows(costtest), costtrain, 'b');
   hold off;
   legend('Teste', 'Treinamento');
   xlabel('Num de amostras');
