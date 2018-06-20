01ab7b06c8c230d2ccccf674037d9df7e39b6991
Mateus S. Vasconcelos <mateus.s.vasconcelos@outlook.com>
2018-06-20 14:09:09 -0300

    syntax error fix

--- IMPLEMENTACAO/ann/random_init.m
+++ IMPLEMENTACAO/ann/random_init.m
@@ -9,7 +9,7 @@ function W = random_init(il_size, hl_size)
   % e depois
   %   pkg install -forge statistics
   W = 0;
-  while( any(any( W == 0 ))
+  while( any(any( W == 0 )) )
     W = stdnormal_rnd(hl_size, il_size+1);
   endwhile  
   % W = randn(il_size, hl_size);
