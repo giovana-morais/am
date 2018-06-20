b11fe30ef1f3bae745a24e3fe018490a85b38178
Giovana Morais <giovana.vmorais@gmail.com>
2018-06-20 08:08:16 -0300

    medida para que não haja pesos iguais a 0

--- IMPLEMENTACAO/ann/random_init.m
+++ IMPLEMENTACAO/ann/random_init.m
@@ -5,10 +5,11 @@ function W = random_init(il_size, hl_size)
   
   % retorna uma matriz de tamanho il_size X hl_size
   % pra funcionar, tem que instalar dois pacotes
-  %   pkg install io-2.4.11.tar.gz
+  %   pkg install -forge io
   % e depois
-  %   pkg install statistics-1.4.0.tar.gz
+  %   pkg install -forge statistics
   
-  W = stdnormal_rnd(hl_size, il_size+1);
+  
+  W = stdnormal_rnd(hl_size, il_size+1) + 1e-7;
   % W = randn(il_size, hl_size);
 endfunction
