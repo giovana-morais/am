d8585b6ab62e1c4c2490db3667d3b53d5d88a82a
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-06-23 13:48:46 -0300

    arrumando path

--- IMPLEMENTACAO/learning_curves.m
+++ IMPLEMENTACAO/learning_curves.m
@@ -3,7 +3,7 @@ pkg load statistics
 if(strcmp(computer(), "x86_64-pc-linux-gnu") )
   addpath("./libsvm/libsvm_x86_64-pc-linux-gnu");
 elseif(ispc())
-  addpath(".\libsvm\libsvm-windows");
+  addpath("./libsvm/libsvm-windows");
 endif
 
 addpath("./ann");
